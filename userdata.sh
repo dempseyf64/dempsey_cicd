74215c4d5c7f7c3a62094826a52b1f6924103269
Finn Dempsey <dempseyf@moravian.edu>
2025-04-08 08:43:02 -0400

    configure repo owner

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install git -y
-git clone https://github.com/cs220s25/dempsey_cicd /dempsey_cicd
+git clone https://github.com/dempseyf64/dempsey_cicd /dempsey_cicd
 cd /dempsey_cicd
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt 
