74bfbb3b6bf076e705965bdd801b34d0166cde15
Finn Dempsey <dempseyf@moravian.edu>
2025-04-08 08:15:38 -0400

    configure repo name

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install git -y
-git clone https://github.com/cs220s25/<repo_name> /<repo_name>
-cd /<repo_name>
+git clone https://github.com/cs220s25/dempsey_cicd /dempsey_cicd
+cd /dempsey_cicd
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt 
 cp web_counter.service /etc/systemd/system
